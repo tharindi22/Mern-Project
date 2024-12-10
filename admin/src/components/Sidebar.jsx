fe9bf685dd755826e0d7352862aba3bc2168bf9f
tharindi22 <ttharindiattanayaka@gmail.com>
2024-12-10 21:37:33 +0530

    Starting create AddDoctors in sidebar of the admin page

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import { AdminContext } from '../context/AdminContext'
 import {assets} from '../assets/assets'
+import { NavLink } from 'react-router-dom'
 
 const Sidebar = () => {
 
@@ -18,17 +19,17 @@ const Sidebar = () => {
             <p>Dashboard</p>
           </NavLink>
 
-          <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}` } to={'/admin-dashboard'} to={'/all-appoinments'}>
+          <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}` } to={'/all-appoinments'}>
             <img src={assets.appointment_icon} alt="" />
             <p>Appoinment</p>
           </NavLink>
 
-          <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}` } to={'/admin-dashboard'} to={'/add-doctor'}>
+          <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}` } to={'/add-doctor'}>
             <img src={assets.add_icon} alt="" />
             <p>Add Doctor</p>
           </NavLink>
 
-          <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}` } to={'/admin-dashboard'} to={'/doctor-list'}>
+          <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}` } to={'/doctor-list'}>
             <img src={assets.people_icon} alt="" />
             <p>Doctor list</p>
           </NavLink>
--- admin/src/pages/Admin/AddDoctor.jsx
+++ admin/src/pages/Admin/AddDoctor.jsx
@@ -1,8 +1,78 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
 
 const AddDoctor = () => {
   return (
-    <div>AddDoctor</div>
+    <form action="">
+      <p>Add Doctor</p>
+
+      <div>
+        <div>
+          <label htmlFor="doc-img ">
+            <img src={assets.upload_area} alt="" />
+          </label>
+          <input type="file" id='doc-img' hidden />
+          <p>Upload doctor <br /> picture </p>
+        </div>
+
+        <div>
+          <div>
+
+            <div>
+              <p>Your name</p>
+              <input type="text" placeholder='Name' required />
+            </div>
+
+            <div>
+              <p>Doctor Email</p>
+              <input type="text" placeholder='Name' required />
+            </div>
+
+            <div>
+              <p>Doctor password</p>
+              <input type="password" placeholder='Password' required />
+            </div>
+
+            <div>
+              <p>Doctor Exprience</p>
+              <select name="" id="">
+                <option value="1 Year">1 Year</option>
+                <option value="2 Year">2 Year</option>
+                <option value="3 Year">3 Year</option>
+                <option value="4 Year">4 Year</option>
+                <option value="5 Year">5 Year</option>
+                <option value="6 Year">6 Year</option>
+                <option value="7 Year">7 Year</option>
+                <option value="8 Year">8 Year</option>
+                <option value="9 Year">9 Year</option>
+                <option value="10 Year">10 Year</option>
+              </select>
+            </div>
+
+            <div>
+              <p>fees</p>
+              <input type="number" placeholder='fees' required />
+            </div>
+
+            <div>
+              <div>
+                <p>Speciality</p>
+                <select name="" id="">
+                  <option value="General physician">General physician</option>
+                  <option value="Gynecologist">Gynecologist</option>
+                  <option value="Dermatologist">Dermatologist</option>
+                  <option value="Pediatricians">Pediatricians</option>
+                  <option value="Neurologist">Neurologist</option>
+                  <option value="Gastroenterologist">Gastroenterologist</option>
+                </select>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+    </form>
   )
 }
 
